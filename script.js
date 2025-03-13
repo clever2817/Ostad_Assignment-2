999e1fd10d6783943db89ab1d08965c528578be5
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-03-13 23:23:42 +0600

    All Problems Solved
    All Problems Solved

--- script.js
+++ script.js
@@ -1,92 +1,100 @@
 // Problem 1
-// function calculateSum(num1, num2) {
-//   return num1 + num2;
-// }
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
 
-// console.log(calculateSum(10, 20));
-// console.log(calculateSum(7, 8));
-// console.log(calculateSum(103, 240));
+console.log(calculateSum(10, 20));
+console.log(calculateSum(7, 8));
+console.log(calculateSum(103, 240));
+
 
 
 // Problem 2
-// let isEven = (number) => {
-//     if (number % 2 === 0)
-//     {
-//         return true;
-//     }
+let isEven = (number) => {
+    if (number % 2 === 0)
+    {
+        return true;
+    }
 
-//     else
-//     {
-//         return false;
-//     }
-// }
+    else
+    {
+        return false;
+    }
+}
 
-// console.log(isEven(10));
-// console.log(isEven(7));
+console.log(isEven(10));
+console.log(isEven(7));
+
 
 
 // Problem 3
-// let numbers = [10, 20, 30, 40, 50, 99, 100];
+let numbers = [10, 20, 30, 40, 50, 99, 100];
 
 
-// function findMax(numbers)
-// {
-//     let maxNum = 0;
+function findMax(numbers)
+{
+    let maxNum = 0;
 
-//     for (let number of numbers) 
-//     {
-//         if(number > maxNum)
-//         {
-//             maxNum = number;
-//         }
-//     }
+    for (let number of numbers) 
+    {
+        if(number > maxNum)
+        {
+            maxNum = number;
+        }
+    }
 
-//     return maxNum;
+    return maxNum;
 
-// }
+}
 
-// console.log(findMax(numbers));
+console.log(findMax(numbers));
+
+
 
 
 // Problem 4
-// let numArr = [4, 2, 5, 6, 1, 9, 15, 24, 23, 17];
+let numArr = [4, 2, 5, 6, 1, 9, 15, 24, 23, 17];
 
 
-// let filterOddNumbers = (numberArray) => {
-//     let oddNumArr = [];
+let filterOddNumbers = (numberArray) => {
+    let oddNumArr = [];
 
-//     for(num of numberArray)
-//     {
-//         if(num % 2 !== 0)
-//         {
-//             oddNumArr.push(num);
-//         }
-//     }
+    for(num of numberArray)
+    {
+        if(num % 2 !== 0)
+        {
+            oddNumArr.push(num);
+        }
+    }
 
-//     return oddNumArr;
-// }
+    return oddNumArr;
+}
 
-// console.log(filterOddNumbers(numArr));
+console.log(filterOddNumbers(numArr));
+
+
 
 
 // Problem 5
-// function countWords(str) 
-// {
-//     let count = 0;
+function countWords(str) 
+{
+    let count = 0;
 
-//     let charArray = Array.from(str);
+    let charArray = Array.from(str);
 
-//     for(let i=1; i<=charArray.length; i++)
-//     {
-//         count++;
-//     }
+    for(let i=1; i<=charArray.length; i++)
+    {
+        count++;
+    }
 
-//     return count;
-// }
+    return count;
+}
 
-// console.log(countWords("Hello")); 
-// console.log(countWords("Bangladesh")); 
-// console.log(countWords("JavaScript")); 
+console.log(countWords("Hello")); 
+console.log(countWords("Bangladesh")); 
+console.log(countWords("JavaScript")); 
+
+
 
 
 // Problem 6
