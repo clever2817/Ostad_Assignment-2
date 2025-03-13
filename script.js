b1cf0f9f8255c9a4cba0f7bd0c0861f138885a43
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-03-13 17:26:00 +0600

    Problem 1  Solved
    Problem 1  Solved

---
+++ script.js
@@ -0,0 +1,10 @@
+// Problem 1
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(calculateSum(10, 20));
+console.log(calculateSum(7, 8));
+console.log(calculateSum(103, 240));
+
+
