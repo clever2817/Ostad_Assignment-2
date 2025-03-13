2a1a48f00f882e53012b00021e7b935219aa8bdd
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-03-13 17:30:48 +0600

    Problem 2 Solved
    Problem 2 Solved

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // Problem 1
-function calculateSum(num1, num2) {
-  return num1 + num2;
+// function calculateSum(num1, num2) {
+//   return num1 + num2;
+// }
+
+// console.log(calculateSum(10, 20));
+// console.log(calculateSum(7, 8));
+// console.log(calculateSum(103, 240));
+
+
+// Problem 2
+let isEven = (number) => {
+    if (number % 2 === 0)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
 }
 
-console.log(calculateSum(10, 20));
-console.log(calculateSum(7, 8));
-console.log(calculateSum(103, 240));
-
-
+console.log(isEven(10));
+console.log(isEven(7));
